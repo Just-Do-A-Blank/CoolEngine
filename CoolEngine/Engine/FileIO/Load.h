792108afcf63e6083352a672d28b7d6f6ef3193f
Thomas-Hodgetts <56154847+Thomas-Hodgetts@users.noreply.github.com>
2022-01-10 16:32:53 +0000

    Updated Load.h to add comments

--- CoolEngine/Engine/FileIO/Load.h
+++ CoolEngine/Engine/FileIO/Load.h
@@ -6,9 +6,18 @@
 
 using namespace nlohmann;
 
+/// <summary>
+/// This class can be used to load files into the engine
+/// </summary>
 class Load
 {
 public:
+
+	/// <summary>
+	/// Takes a file location and then returns a nlohmann::json. Throws an exception in the visual studios output channel if it has failed.
+	/// </summary>
+	/// <param name="fileAddress"></param>
+	/// <returns></returns>
 	static json Json(const char* fileAddress);
 
 };
