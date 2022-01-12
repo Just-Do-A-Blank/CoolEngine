93aa103e368512cd621aefc4852d18f04c52cf98
Joe Finn <joefinn01@hotmail.com>
2022-01-12 11:22:47 +0000

    Adding basic functionality to renderable game object class

--- CoolEngine/Engine/GameObjects/RenderableGameObject.cpp
+++ CoolEngine/Engine/GameObjects/RenderableGameObject.cpp
@@ -1,1 +1,122 @@
 #include "RenderableGameObject.h"
+#include "Engine/Graphics/GraphicsManager.h"
+
+#include <iostream>
+
+Mesh* RenderableGameObject::GetMesh() const
+{
+	return m_pmesh;
+}
+
+ID3D11ShaderResourceView* RenderableGameObject::GetAlbedoSRV() const
+{
+	return m_palbedoSRV;
+}
+
+const PerInstanceCB& RenderableGameObject::GetCB() const
+{
+	return m_cb;
+}
+
+ID3D11VertexShader* RenderableGameObject::GetVertexShader() const
+{
+	return m_pvertexShader;
+}
+
+ID3D11PixelShader* RenderableGameObject::GetPixelShader() const
+{
+	return m_ppixelShader;
+}
+
+bool RenderableGameObject::SetMesh(std::wstring meshName)
+{
+	Mesh* pmesh = GraphicsManager::GetInstance()->GetMesh(meshName);
+
+	if (pmesh == nullptr)
+	{
+		cout << "Failed to set the mesh as one with that name doesn't exist!" << endl;
+
+		return false;
+	}
+
+	m_pmesh = pmesh;
+
+	return true;
+}
+
+void RenderableGameObject::SetMesh(Mesh* pmesh)
+{
+	m_pmesh = pmesh;
+}
+
+bool RenderableGameObject::SetAlbedo(std::wstring albedoName)
+{
+	ID3D11ShaderResourceView* psRV = GraphicsManager::GetInstance()->GetShaderResourceView(albedoName);
+
+	if (psRV == nullptr)
+	{
+		cout << "Failed to set the albedo SRV as one with that name doesn't exist!" << endl;
+
+		return false;
+	}
+
+	m_palbedoSRV = psRV;
+
+	return true;
+}
+
+void RenderableGameObject::SetAlbedo(ID3D11ShaderResourceView* psRV)
+{
+	m_palbedoSRV = psRV;
+}
+
+void RenderableGameObject::SetCB(PerInstanceCB& cb)
+{
+	m_cb = cb;
+}
+
+bool RenderableGameObject::SetVertexShader(std::wstring shaderName)
+{
+	ID3D11VertexShader* pvertexShader = GraphicsManager::GetInstance()->GetVertexShader(shaderName);
+
+	if (pvertexShader == nullptr)
+	{
+		std::cout << "Failed to find vertex shader with that name " << shaderName.c_str() << "!" << std::endl;
+
+		return false;
+	}
+
+	m_pvertexShader = pvertexShader;
+
+	return true;
+}
+
+void RenderableGameObject::SetVertexShader(ID3D11VertexShader* pvertexShader)
+{
+	m_pvertexShader = pvertexShader;
+}
+
+bool RenderableGameObject::SetPixelShader(std::wstring shaderName)
+{
+	ID3D11PixelShader* ppixelShader = GraphicsManager::GetInstance()->GetPixelShader(shaderName);
+
+	if (ppixelShader == nullptr)
+	{
+		std::cout << "Failed to find pixel shader with that name " << shaderName.c_str() << "!" << std::endl;
+
+		return false;
+	}
+
+	m_ppixelShader = ppixelShader;
+
+	return true;
+}
+
+void RenderableGameObject::SetPixelShader(ID3D11PixelShader* ppixelShader)
+{
+	m_ppixelShader = ppixelShader;
+}
+
+void RenderableGameObject::Render()
+{
+}
