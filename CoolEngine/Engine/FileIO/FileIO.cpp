f72aaf9c13e902ea7e426c295734e31cf228b5a0
Thomas-Hodgetts <56154847+Thomas-Hodgetts@users.noreply.github.com>
2022-01-12 17:13:38 +0000

    12th January work
    Merged the save and load classes into a single FileIO class.
    
    Intergrated the GameObject class into the loading system but have no way to access the object itself

---
+++ CoolEngine/Engine/FileIO/FileIO.cpp
@@ -0,0 +1,187 @@
+#include "FileIO.h"
+
+COLLIDER_TYPE FileIO::ColliderType(std::string colliderType)
+{
+	if (colliderType == "BoxCollider")
+	{
+		return COLLIDER_TYPE::COLLIDER_TYPE_BOX;
+	}
+	else if (colliderType == "CircleCollider")
+	{
+		return COLLIDER_TYPE::COLLIDER_TYPE_CIRCLE;
+	}
+	else if(colliderType == "LineCollider")
+	{
+		return COLLIDER_TYPE::COLLIDER_TYPE_LINE;
+	}
+	else
+	{
+		OutputDebugStringA("No Matching Collider");
+		return COLLIDER_TYPE::COLLIDER_TYPE_NO_COLLIDER;
+	}
+
+}
+
+json FileIO::LoadJson(const char* fileAddress)
+{
+	std::ifstream file(fileAddress);
+	try
+	{
+		if (file)
+		{
+			json j;
+			file >> j;
+			return j;
+		}
+		throw std::exception("File was not opened");
+	}
+	catch (const std::exception& e)
+	{
+		OutputDebugStringA(e.what());
+		return json();
+	}
+}
+
+std::vector<GameObjectData> FileIO::LoadMultipleGameObjects(const char* fileAddress)
+{
+	json m_json = LoadJson(fileAddress);
+	json data = m_json["ObjectData"];
+	json metaData = m_json["MetaData"];
+
+	std::vector<GameObjectData> objects(metaData.at(0)["NumberOfObject"]);
+
+	for (size_t i = 0; i < metaData.at(0)["NumberOfObject"]; i++)
+	{
+		objects[i].m_Position = XMFLOAT3(data.at(0)["Position"][0], data.at(0)["Position"][1], data.at(0)["Position"][2]);
+		objects[i].m_Rotation = XMFLOAT3(data.at(0)["Rotation"][0], data.at(0)["Rotation"][1], data.at(0)["Rotation"][2]);
+		objects[i].m_Scale = XMFLOAT3(data.at(0)["Scale"][0], data.at(0)["Scale"][1], data.at(0)["Scale"][2]);
+
+		if (data.at(0)["IsRenderable"])
+		{
+			objects[i].m_TextureData = nullptr;
+			objects[i].m_IsRenderable = true;
+		}
+		else
+		{
+			objects[i].m_TextureData = nullptr;
+			objects[i].m_IsRenderable = false;
+		}
+		if (data.at(0)["IsTrigger"])
+		{
+			objects[i].m_IsTrigger = true;
+			objects[i].m_ColliderType = ColliderType(data.at(0)["ColliderType"]);
+		}
+		else
+		{
+			objects[i].m_IsTrigger = false;
+		}
+		if (data.at(0)["IsCollideable"])
+		{
+			objects[i].m_IsCollidable = true;
+			objects[i].m_ColliderType = ColliderType(data.at(0)["ColliderType"]);
+		}
+		else
+		{
+			objects[i].m_IsCollidable = false;
+		}
+	}
+}
+
+GameObjectData FileIO::LoadGameObject(const char* fileAddress)
+{
+	json m_json = LoadJson(fileAddress);
+	json data = m_json["ObjectData"];
+
+	GameObjectData gameObjData;
+	gameObjData.m_Position = XMFLOAT3(data.at(0)["Position"][0], data.at(0)["Position"][1], data.at(0)["Position"][2]);
+	gameObjData.m_Rotation = XMFLOAT3(data.at(0)["Rotation"][0], data.at(0)["Rotation"][1], data.at(0)["Rotation"][2]) ;
+	gameObjData.m_Scale = XMFLOAT3(data.at(0)["Scale"][0], data.at(0)["Scale"][1], data.at(0)["Scale"][2]);
+
+	if (data.at(0)["IsRenderable"])
+	{
+		gameObjData.m_TextureData = nullptr;
+		gameObjData.m_IsRenderable = true;
+	}
+	else
+	{
+		gameObjData.m_TextureData = nullptr;
+		gameObjData.m_IsRenderable = false;
+	}
+	if (data.at(0)["IsTrigger"])
+	{
+		gameObjData.m_IsTrigger = true;
+		gameObjData.m_ColliderType = ColliderType(data.at(0)["ColliderType"]);
+	}
+	else
+	{
+		gameObjData.m_IsTrigger = false;
+	}
+	if (data.at(0)["IsCollideable"])
+	{
+		gameObjData.m_IsCollidable = true;
+		gameObjData.m_ColliderType = ColliderType(data.at(0)["ColliderType"]);
+	}
+	else
+	{
+		gameObjData.m_IsCollidable = false;
+	}
+	return gameObjData;
+}
+
+void FileIO::LoadMap(const char* fileAddress)
+{
+
+}
+
+void FileIO::LoadSavefile(const char* fileAddress)
+{
+
+}
+
+//////////////////////////SAVE FUNCTIONS ////////////////////////////////////////
+
+void FileIO::SaveGameObject(GameObjectData gameObjData)
+{
+
+	std::ofstream outFile;
+	outFile.open("TestFile.json");
+
+	std::string colliderType;
+
+	switch (gameObjData.m_ColliderType)
+	{
+	default:
+		colliderType = "NoCollider";
+		break;
+	case COLLIDER_TYPE::COLLIDER_TYPE_BOX:
+		colliderType = "BoxCollider";
+		break;
+	case COLLIDER_TYPE::COLLIDER_TYPE_CIRCLE:
+		colliderType = "CircleCollider";
+		break;
+	case COLLIDER_TYPE::COLLIDER_TYPE_LINE:
+		colliderType = "LineCollider";
+		break;
+	}
+	float position[3] = {gameObjData.m_Position.x, gameObjData.m_Position.y , gameObjData.m_Position.z };
+	float rotation[3] = {gameObjData.m_Rotation.x, gameObjData.m_Rotation.y , gameObjData.m_Rotation.z };
+	float scale[3] = {gameObjData.m_Scale.x, gameObjData.m_Scale.y , gameObjData.m_Scale.z };
+
+	json m_JsonOutput
+	{
+		{"Metadata",
+			{
+				{"ColliderType", colliderType},
+				{"IsCollideable" , gameObjData.m_IsCollidable},
+				{"IsRenderable", gameObjData.m_IsRenderable},
+				{"IsTrigger", gameObjData.m_IsTrigger},
+				{"Position" , position},
+				{"Rotation", rotation } ,
+				{"Scale" , scale} ,
+				{"TextureLocation", "ATexLocation"}
+			}
+		}
+	};
+	outFile << m_JsonOutput;
+	outFile.close();
+}
---
+++ CoolEngine/Engine/FileIO/FileIO.h
@@ -0,0 +1,117 @@
+#pragma once
+
+#include "json.hpp"
+#include "Engine/Managers/GameManager.h"
+#include <fstream>
+#include <iostream>
+#include <windows.h>
+
+using namespace nlohmann;
+
+/// <summary>
+/// Flags that can be used to determin an objects collider type
+/// </summary>
+enum COLLIDER_TYPE
+{
+	COLLIDER_TYPE_BOX,
+	COLLIDER_TYPE_CIRCLE,
+	COLLIDER_TYPE_LINE,
+	COLLIDER_TYPE_NO_COLLIDER
+};
+
+/// <summary>
+/// A struct that contains data related to a game object
+/// </summary>
+struct GameObjectData
+{
+	GameObjectData() 
+	{ 
+		m_Position = XMFLOAT3();
+		m_Rotation = XMFLOAT3();
+		m_Scale = XMFLOAT3();
+		m_TextureData = nullptr; 
+		m_IsRenderable = false;
+		m_IsCollidable = false;
+		m_IsTrigger = false;
+		m_ColliderType = COLLIDER_TYPE_NO_COLLIDER;
+	};
+
+	XMFLOAT3 m_Position, m_Rotation, m_Scale;
+	bool m_IsRenderable, m_IsCollidable, m_IsTrigger;
+	ID3D11Resource* m_TextureData;
+	COLLIDER_TYPE m_ColliderType;
+};
+
+/// <summary>
+/// A struct that contains data related to save data
+/// </summary>
+struct SaveFileData
+{
+	SaveFileData() {};
+};
+
+/// <summary>
+/// A struct that contains data related to a map file
+/// </summary>
+struct MapFileData
+{
+	MapFileData() {};
+};
+
+/// <summary>
+/// This class can be used to load & save files in the engine
+/// </summary>
+class FileIO
+{
+public:
+
+	/// <summary>
+	/// Loads a Game Object from a JSON file.
+	/// </summary>
+	/// <param name="fileAddress"></param>
+	/// <param name="manager"></param>
+	/// <returns></returns>
+	static GameObjectData LoadGameObject(const char* fileAddress);
+
+	/// <summary>
+	/// Loads multiple game objects from a singular JSON file
+	/// </summary>
+	/// <param name="fileAddress"></param>
+	/// <returns>std::vector<GameObjectData></returns>
+	static std::vector<GameObjectData> LoadMultipleGameObjects(const char* fileAddress);
+
+	/// <summary>
+	/// Loads the map from a JSON file. Needs a file address to do so. Code in this function is temorary until the structure of the object that needs loading is made clear
+	/// </summary>
+	/// <param name="fileAddress"></param>
+	/// <returns></returns>
+	static void LoadMap(const char* fileAddress);
+
+	/// <summary>
+	/// Loads a Save File from a JSON file. Needs a file address to do so. Code in this function is temorary until the structure of the object that needs loading is made clear
+	/// </summary>
+	/// <param name="fileAddress"></param>
+	/// <returns></returns>
+	static void LoadSavefile(const char* fileAddress);
+
+	/// <summary>
+	/// A function that saves data related to a Game Object and saves it to a JSON file
+	/// </summary>
+	/// <param name="gameObjData"></param>
+	static void SaveGameObject(GameObjectData gameObjData);
+
+private:
+	/// <summary>
+	/// Takes a file location and then returns a nlohmann::json. Throws an exception in the visual studios output channel if it has failed.
+	/// </summary>
+	/// <param name="char* fileAddress"></param>
+	/// <returns></returns>
+	static json LoadJson(const char* fileAddress);
+
+	/// <summary>
+	/// Determins what collider type a object has from a string
+	/// </summary>
+	/// <param name="colliderType"></param>
+	/// <returns>COLLIDER_TYPE</returns>
+	static COLLIDER_TYPE ColliderType(std::string colliderType);
+};
