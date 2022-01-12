5aec415ec576c04ec831c1b102a578b8fff03b4d
Joe Finn <joefinn01@hotmail.com>
2022-01-12 16:31:20 +0000

    Adding graphics components to gameobject

--- CoolEngine/Engine/GameObjects/GameObject.cpp
+++ CoolEngine/Engine/GameObjects/GameObject.cpp
@@ -1,5 +1,9 @@
 #include "GameObject.h"
 
+#include "Engine/Managers/GraphicsManager.h"
+
+#include <iostream>
+
 void GameObject::CreateRenderableGameObject()
 {
 	m_isRenderable = true;
@@ -33,4 +37,139 @@ const bool& GameObject::IsCollidable()
 const bool& GameObject::IsTrigger()
 {
 	return m_isTrigger;
+}
+
+void GameObject::Render(ID3D11DeviceContext* pcontext, ConstantBuffer<PerInstanceCB>* pconstantBuffer)
+{
+	//Update CB
+	PerInstanceCB cb;
+	XMStoreFloat4x4(&cb.world, XMMatrixTranspose(m_transform.GetWorldMatrix()));
+
+	pconstantBuffer->Update(cb, pcontext);
+
+	ID3D11Buffer* pcbBuffer = pconstantBuffer->GetBuffer();
+
+	//Bind CB and appropriate resources
+	pcontext->VSSetConstantBuffers((int)GraphicsManager::CBOrders::PER_INSTANCE, 1, &pcbBuffer);
+	pcontext->VSSetShader(m_pvertexShader, nullptr, 0);
+
+	pcontext->PSSetConstantBuffers((int)GraphicsManager::CBOrders::PER_INSTANCE, 1, &pcbBuffer);
+	pcontext->PSSetShaderResources(0, 1, &m_palbedoSRV);
+	pcontext->PSSetShader(m_ppixelShader, nullptr, 0);
+
+	//Draw object
+	pcontext->DrawIndexed(m_pmesh->GetIndexCount(), 0, 0);
+
+	//Unbind resources
+	pcontext->VSSetConstantBuffers((int)GraphicsManager::CBOrders::PER_INSTANCE, 0, nullptr);
+	pcontext->PSSetConstantBuffers((int)GraphicsManager::CBOrders::PER_INSTANCE, 0, nullptr);
+}
+
+Mesh* GameObject::GetMesh() const
+{
+	return m_pmesh;
+}
+
+ID3D11ShaderResourceView* GameObject::GetAlbedoSRV() const
+{
+	return m_palbedoSRV;
+}
+
+ID3D11VertexShader* GameObject::GetVertexShader() const
+{
+	return m_pvertexShader;
+}
+
+ID3D11PixelShader* GameObject::GetPixelShader() const
+{
+	return m_ppixelShader;
+}
+
+Transform* GameObject::GetTransform()
+{
+	return &m_transform;
+}
+
+bool GameObject::SetMesh(std::wstring meshName)
+{
+	Mesh* pmesh = GraphicsManager::GetInstance()->GetMesh(meshName);
+
+	if (pmesh == nullptr)
+	{
+		cout << "Failed to set the mesh as one with that name doesn't exist!" << endl;
+
+		return false;
+	}
+
+	m_pmesh = pmesh;
+
+	return true;
+}
+
+void GameObject::SetMesh(Mesh* pmesh)
+{
+	m_pmesh = pmesh;
+}
+
+bool GameObject::SetAlbedo(std::wstring albedoName)
+{
+	ID3D11ShaderResourceView* psRV = GraphicsManager::GetInstance()->GetShaderResourceView(albedoName);
+
+	if (psRV == nullptr)
+	{
+		cout << "Failed to set the albedo SRV as one with that name doesn't exist!" << endl;
+
+		return false;
+	}
+
+	m_palbedoSRV = psRV;
+
+	return true;
+}
+
+void GameObject::SetAlbedo(ID3D11ShaderResourceView* psRV)
+{
+	m_palbedoSRV = psRV;
+}
+
+bool GameObject::SetVertexShader(std::wstring shaderName)
+{
+	ID3D11VertexShader* pvertexShader = GraphicsManager::GetInstance()->GetVertexShader(shaderName);
+
+	if (pvertexShader == nullptr)
+	{
+		std::cout << "Failed to find vertex shader with that name " << shaderName.c_str() << "!" << std::endl;
+
+		return false;
+	}
+
+	m_pvertexShader = pvertexShader;
+
+	return true;
+}
+
+void GameObject::SetVertexShader(ID3D11VertexShader* pvertexShader)
+{
+	m_pvertexShader = pvertexShader;
+}
+
+bool GameObject::SetPixelShader(std::wstring shaderName)
+{
+	ID3D11PixelShader* ppixelShader = GraphicsManager::GetInstance()->GetPixelShader(shaderName);
+
+	if (ppixelShader == nullptr)
+	{
+		std::cout << "Failed to find pixel shader with that name " << shaderName.c_str() << "!" << std::endl;
+
+		return false;
+	}
+
+	m_ppixelShader = ppixelShader;
+
+	return true;
+}
+
+void GameObject::SetPixelShader(ID3D11PixelShader* ppixelShader)
+{
+	m_ppixelShader = ppixelShader;
 }
--- CoolEngine/Engine/GameObjects/GameObject.h
+++ CoolEngine/Engine/GameObjects/GameObject.h
@@ -1,12 +1,23 @@
 #pragma once
 #include "Transform.h"
 #include "Engine/Managers/GameManager.h"
+#include "Engine/Graphics/Mesh.h"
+#include "Engine/Graphics/ConstantBuffer.h"
+#include "Engine/Graphics/ConstantBuffers.h"
+
+#include <string>
 
 class GameObject
 {
 	friend class GameManager;
 private:
-	ID3D11ShaderResourceView* m_texture;
+	//Graphics variables
+	ID3D11ShaderResourceView* m_palbedoSRV;
+
+	ID3D11VertexShader* m_pvertexShader;
+	ID3D11PixelShader* m_ppixelShader;
+
+	Mesh* m_pmesh;
 
 	//Flags
 	bool m_isRenderable = false;
@@ -27,4 +38,29 @@ public:
 	const bool& IsRenderable();
 	const bool& IsCollidable();
 	const bool& IsTrigger();
+
+	void Render(ID3D11DeviceContext* pcontext, ConstantBuffer<PerInstanceCB>* pconstantBuffer);
+
+	//Getters
+	Mesh* GetMesh() const;
+
+	ID3D11ShaderResourceView* GetAlbedoSRV() const;
+
+	ID3D11VertexShader* GetVertexShader() const;
+	ID3D11PixelShader* GetPixelShader() const;
+
+	Transform* GetTransform();
+
+	//Setters
+	bool SetMesh(wstring meshName);
+	void SetMesh(Mesh* pmesh);
+
+	bool SetAlbedo(wstring albedoName);
+	void SetAlbedo(ID3D11ShaderResourceView* psRV);
+
+	bool SetVertexShader(wstring shaderName);
+	void SetVertexShader(ID3D11VertexShader* pvertexShader);
+
+	bool SetPixelShader(wstring shaderName);
+	void SetPixelShader(ID3D11PixelShader* ppixelShader);
 };
