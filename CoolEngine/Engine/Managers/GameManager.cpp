1f549642e001e15a6eb46e072ac2920094fb8922
Joe Finn <joefinn01@hotmail.com>
2022-01-13 12:40:39 +0000

    Adding timer to game manager

--- CoolEngine/Engine/Managers/GameManager.cpp
+++ CoolEngine/Engine/Managers/GameManager.cpp
@@ -1,1 +1,11 @@
 #include "GameManager.h"
+
+GameObject* GameManager::GetGameObject(string name)
+{
+    return m_gameObjectMap[name];
+}
+
+Timer* GameManager::GetTimer()
+{
+    return &m_timer;
+}
