764351c1205ad64c811c30d53dd32357e69be15b
Max Osman <57242759+MaxOsman@users.noreply.github.com>
2022-01-13 16:02:45 +0000

    Collision Tweak
    Should now work with the Y-Z axis switch. If not then yell at me

--- CoolEngine/Engine/Physics/Collision.cpp
+++ CoolEngine/Engine/Physics/Collision.cpp
@@ -7,10 +7,10 @@
 
 bool Collision::BoxCollision(Transform* t1, Transform* t2)
 {
-	XMFLOAT2 halfSize1 = { t1->GetScale().x, t1->GetScale().z };
-	XMFLOAT2 halfSize2 = { t2->GetScale().x, t2->GetScale().z };
-	XMFLOAT2 middle1 = { t1->GetPosition().x, t1->GetPosition().z };
-	XMFLOAT2 middle2 = { t2->GetPosition().x, t2->GetPosition().z };
+	XMFLOAT2 halfSize1 = { t1->GetScale().x, t1->GetScale().y };
+	XMFLOAT2 halfSize2 = { t2->GetScale().x, t2->GetScale().y };
+	XMFLOAT2 middle1 = { t1->GetPosition().x, t1->GetPosition().y };
+	XMFLOAT2 middle2 = { t2->GetPosition().x, t2->GetPosition().y };
 
 	return (middle1.x + halfSize1.x > middle2.x - halfSize2.x && middle1.x - halfSize1.x < middle2.x + halfSize2.x && middle1.y + halfSize1.y > middle2.y - halfSize2.y && middle1.y - halfSize1.y < middle2.y + halfSize2.y);
 }
@@ -53,10 +53,10 @@ bool Collision::CircleBoxCollision(Circle circle, Box box)
 
 bool Collision::BoxCollisionAndResponse(Transform* player, Transform* object)
 {
-	XMFLOAT2 halfSizeP = { player->GetScale().x, player->GetScale().z };
-	XMFLOAT2 halfSizeO = { object->GetScale().x, object->GetScale().z };
-	XMFLOAT2 middleP = { player->GetPosition().x, player->GetPosition().z };
-	XMFLOAT2 middleO = { object->GetPosition().x, object->GetPosition().z };
+	XMFLOAT2 halfSizeP = { player->GetScale().x, player->GetScale().y };
+	XMFLOAT2 halfSizeO = { object->GetScale().x, object->GetScale().y };
+	XMFLOAT2 middleP = { player->GetPosition().x, player->GetPosition().y };
+	XMFLOAT2 middleO = { object->GetPosition().x, object->GetPosition().y };
 
 	if (middleP.x + halfSizeP.x > middleO.x - halfSizeO.x && middleP.x - halfSizeP.x < middleO.x + halfSizeO.x && middleP.y + halfSizeP.y > middleO.y - halfSizeO.y && middleP.y - halfSizeP.y < middleO.y + halfSizeO.y)
 	{
@@ -68,13 +68,13 @@ bool Collision::BoxCollisionAndResponse(Transform* player, Transform* object)
 		if (vertexToPlayer.y > 0 && vertexToPlayer.x < vertexToPlayer.y && vertexToPlayer.x > vertexToPlayer.y * -1)
 		{
 			// Up side
-			XMFLOAT3 pos = { player->GetPosition().x, player->GetPosition().y, middleO.y + halfSizeO.y + halfSizeP.y };
+			XMFLOAT3 pos = { player->GetPosition().x, middleO.y + halfSizeO.y + halfSizeP.y, player->GetPosition().z };
 			player->SetPosition(pos);
 		}
 		else if (vertexToPlayer.y < 0 && vertexToPlayer.x > vertexToPlayer.y && vertexToPlayer.x < vertexToPlayer.y * -1)
 		{
 			// Down side
-			XMFLOAT3 pos = { player->GetPosition().x, player->GetPosition().y, middleO.y - halfSizeO.y - halfSizeP.y };
+			XMFLOAT3 pos = { player->GetPosition().x, middleO.y - halfSizeO.y - halfSizeP.y, player->GetPosition().z };
 			player->SetPosition(pos);
 		}
 		else if (vertexToPlayer.x > 0 && vertexToPlayer.y < vertexToPlayer.x && vertexToPlayer.y > vertexToPlayer.x * -1)
@@ -94,13 +94,13 @@ bool Collision::BoxCollisionAndResponse(Transform* player, Transform* object)
 			if (vertexToPlayer.x > 0)
 			{
 				// Top right corner
-				XMFLOAT3 pos = { middleO.x + halfSizeO.x + halfSizeP.x, player->GetPosition().y, middleO.y + halfSizeO.y + halfSizeP.y };
+				XMFLOAT3 pos = { middleO.x + halfSizeO.x + halfSizeP.x, middleO.y + halfSizeO.y + halfSizeP.y, player->GetPosition().z };
 				player->SetPosition(pos);
 			}
 			else if(vertexToPlayer.x < 0)
 			{
 				// Bottom left corner
-				XMFLOAT3 pos = { middleO.x - halfSizeO.x - halfSizeP.x, player->GetPosition().y, middleO.y - halfSizeO.y - halfSizeP.y };
+				XMFLOAT3 pos = { middleO.x - halfSizeO.x - halfSizeP.x, middleO.y - halfSizeO.y - halfSizeP.y, player->GetPosition().z };
 				player->SetPosition(pos);
 			}
 		}
@@ -109,13 +109,13 @@ bool Collision::BoxCollisionAndResponse(Transform* player, Transform* object)
 			if (vertexToPlayer.x > 0)
 			{
 				// Bottom right corner
-				XMFLOAT3 pos = { middleO.x + halfSizeO.x + halfSizeP.x, player->GetPosition().y, middleO.y - halfSizeO.y - halfSizeP.y };
+				XMFLOAT3 pos = { middleO.x + halfSizeO.x + halfSizeP.x, middleO.y - halfSizeO.y - halfSizeP.y, player->GetPosition().z };
 				player->SetPosition(pos);
 			}
 			else if (vertexToPlayer.x < 0)
 			{
 				// Top left corner
-				XMFLOAT3 pos = { middleO.x - halfSizeO.x - halfSizeP.x, player->GetPosition().y, middleO.y + halfSizeO.y + halfSizeP.y };
+				XMFLOAT3 pos = { middleO.x - halfSizeO.x - halfSizeP.x, middleO.y + halfSizeO.y + halfSizeP.y, player->GetPosition().z };
 				player->SetPosition(pos);
 			}
 		}
@@ -123,7 +123,7 @@ bool Collision::BoxCollisionAndResponse(Transform* player, Transform* object)
 		{
 			// Default to top side
 			// Should only happen if player is somehow at exact position of object
-			XMFLOAT3 pos = { player->GetPosition().x, player->GetPosition().y, middleO.y + halfSizeO.y + halfSizeP.y };
+			XMFLOAT3 pos = { player->GetPosition().x, middleO.y + halfSizeO.y + halfSizeP.y, player->GetPosition().z };
 			player->SetPosition(pos);
 		}
 
