cfbc2202168e5d3e57dc6ad22cab2a23e01066d7
Abhiroop Basak <46960936+Abhiroop-Basak@users.noreply.github.com>
2022-01-13 16:41:14 +0000

    Removing GetGameObject from GameManager

--- CoolEngine/Engine/Managers/GameManager.cpp
+++ CoolEngine/Engine/Managers/GameManager.cpp
@@ -1,10 +1,5 @@
 #include "GameManager.h"
 
-GameObject* GameManager::GetGameObject(string name)
-{
-    //return m_gameObjectMap[name];
-}
-
 Timer* GameManager::GetTimer()
 {
     return &m_timer;
