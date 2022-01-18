501501ebe730cede2ec135d641fbf71cc1439b67
Joe Finn <joefinn01@hotmail.com>
2022-01-18 17:51:38 +0000

    Upgrading log functionality to show class and line

--- CoolEngine/Engine/Helpers/DebugHelper.h
+++ CoolEngine/Engine/Helpers/DebugHelper.h
@@ -1,7 +1,22 @@
 #pragma once
 
+#include <string>
+
+static std::string className(const std::string& prettyFunction)
+{
+	size_t colons = prettyFunction.find("::");
+	if (colons == std::string::npos)
+		return "::";
+	size_t begin = prettyFunction.substr(0, colons).rfind(" ") + 1;
+	size_t end = colons - begin;
+
+	return prettyFunction.substr(begin, end);
+}
+
+#define __CLASS_NAME__ className(__FUNCSIG__)
+
 #if _DEBUG 
-#define LOG(message)cout<<message<<endl;
+#define LOG(message)cout<< __CLASS_NAME__ << ", " << __LINE__ << ": " << message <<endl;
 #else
 #define LOG(message)
 #endif
