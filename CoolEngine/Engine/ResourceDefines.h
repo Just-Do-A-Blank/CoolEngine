2b245551152ecb84a6d6fb1e92606992c67d2bf3
Abhiroop Basak <46960936+Abhiroop-Basak@users.noreply.github.com>
2022-01-21 11:06:18 +0000

    Finishing world-space Debug Draw

--- CoolEngine/Engine/ResourceDefines.h
+++ CoolEngine/Engine/ResourceDefines.h
@@ -3,7 +3,9 @@
 //Sprites
 #define DEFAULT_IMGUI_IMAGE L"Resources\\Sprites\\Brick.dds"
 #define TEST2 L"Resources\\Sprites\\Test2.dds"
-#define DEBUG_COLOUR L"Resources\\Debug\\DebugColour"
+
+//Debug
+#define DEBUG_COLOUR L"Resources\\DebugColours\\DebugColour"
 
 //Animations
 #define TEST_ANIM L"TestAnim"
