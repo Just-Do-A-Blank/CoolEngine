5b88f3887fc1493a73cfb49ff5b5f086bf31a85b
Joe Finn <joefinn01@hotmail.com>
2022-01-26 10:06:48 +0000

    Started implementing the tilemap tool

--- CoolEngine/Engine/Tools/TileMapTool.cpp
+++ CoolEngine/Engine/Tools/TileMapTool.cpp
@@ -5,6 +5,14 @@
 #include "Engine/Includes/IMGUI/imgui.h"
 #include "Engine/Includes/IMGUI/imgui_impl_win32.h"
 #include "Engine/Includes/IMGUI/imgui_impl_dx11.h"
+#include "Engine/EditorUI/EditorUI.h"
+#include "Engine/Tools/TileMapTool.h"
+#include "Engine/TileMap/TileMap/TileMap.h"
+
+void TileMapTool::Init(ID3D11Device* pdevice)
+{
+	ToolBase::Init(pdevice);
+}
 
 void TileMapTool::Render()
 {
@@ -12,14 +20,47 @@ void TileMapTool::Render()
 	ImGui_ImplWin32_NewFrame();
 	ImGui::NewFrame();
 
-	ImGui::Begin("test");
-	ImGui::End();
+	if (m_selectingDimensions == true)
+	{
+		CreateSelectDimensionsUI();
+	}
+	else
+	{
+		ImGui::Begin("Properties");
+
+		ImGui::End();
+	}
 
 	ImGui::Render();
 
 	ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
 
 	ImGui::EndFrame();
+}
+
+void TileMapTool::Update()
+{
+	if (m_selectingDimensions == true)
+	{
+		return;
+	}
+}
+
+void TileMapTool::CreateSelectDimensionsUI()
+{
+	ImGui::Begin("Tile Map Properties");
+
+	EditorUI::DragInt("Width", m_tileMapWidth);
+	EditorUI::DragInt("Height", m_tileMapHeight);
+
+	if (ImGui::Button("Create") == true)
+	{
+		m_ptileMap = new TileMap(m_tileMapWidth, m_tileMapHeight, "TileMap", XMFLOAT3(0, 0, 0));
+
+		m_selectingDimensions = false;
+	}
+
+	ImGui::End();
 }
 
 #endif
--- CoolEngine/Engine/Tools/TileMapTool.h
+++ CoolEngine/Engine/Tools/TileMapTool.h
@@ -3,15 +3,29 @@
 #if EDITOR
 
 #include "ToolBase.h"
+
+class TileMap;
+
 class TileMapTool : public ToolBase
 {
 public:
 
+	virtual void Init(ID3D11Device* pdevice);
+	void Render() override;
+	void Update() override;
+
 protected:
-	virtual void Render() override;
+
 
 private:
+	void CreateSelectDimensionsUI();
 
+	TileMap* m_ptileMap = nullptr;
+
+	bool m_selectingDimensions = true;
+
+	int m_tileMapWidth = -1;
+	int m_tileMapHeight = -1;
 };
 
 #endif
