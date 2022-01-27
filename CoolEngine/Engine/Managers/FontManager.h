b44a536aa3b419ee5592fe28bbf5dabaf8bbacac
Abhiroop Basak <46960936+Abhiroop-Basak@users.noreply.github.com>
2022-01-27 05:38:56 +0000

    Adding FontManager class
    This class is used to load and store Font Atlases

--- CoolEngine/Engine/Managers/FontManager.h
+++ CoolEngine/Engine/Managers/FontManager.h
@@ -6,13 +6,27 @@ struct FontAtlasStruct
 	int charId;
 	XMFLOAT2 charTexPosition;
 	XMFLOAT2 charDimension;
+	XMFLOAT3 spacing;
+
+	ID3D11ShaderResourceView* fontTexture = nullptr;
 };
 
 class FontManager : public Singleton<FontManager>
 {
 private:
-
+	unordered_map<string, vector<FontAtlasStruct*>> m_fontAtlasMap;
+	unordered_map<string, XMINT2> m_fontTextureDimension;
+	vector<string>m_fontList;
+	unordered_map<string, wstring>m_fontTexturePathMap;
 public:
-	void LoadFonts();
+	void LoadFont(string fontInfoFilePath, wstring fontTextureFilePath, string fontName);
+
+	FontAtlasStruct* GetFontCharacterData(string fontName, char character);
+	vector<FontAtlasStruct*>& GetFontAtlas(string fontName, int fontSize);
+	XMINT2& GetTextureDimensionOfFont(string fontName);
+	wstring& GetFontTextureFilePath(string fontName);
+	void UpdateVertexBuffer();
+	vector<string>& GetFontNames();
+
 };
 
