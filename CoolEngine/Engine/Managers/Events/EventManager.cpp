23d9dedb3cb2eac82a90828549e0a82dc83badb6
Joe Finn <joefinn01@hotmail.com>
2022-05-07 14:20:38 +0100

    Removing commented code

--- CoolEngine/Engine/Managers/Events/EventManager.cpp
+++ CoolEngine/Engine/Managers/Events/EventManager.cpp
@@ -27,7 +27,6 @@ void EventManager::AddClient(EventType eventid, Observer* client)
 	if (!IsRegistered(eventid, client))
 	{
 		m_listenerClients[eventid].push_back(client);
-		//m_listenerClients.insert(make_pair(eventid, client));
 	}
 	
 	else //Outputs that there is a duplicate and doesnt add it to the list of m_listenerClients since it has no value to have 2
