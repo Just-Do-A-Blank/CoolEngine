1b1d879194b38936a485588e3af6f69b236cdebc
Abhiroop Basak <46960936+Abhiroop-Basak@users.noreply.github.com>
2022-05-14 13:47:37 +0100

    Finishing ButtonUIComponent

--- CoolEngine/Engine/GameUI/ButtonComponent.cpp
+++ CoolEngine/Engine/GameUI/ButtonComponent.cpp
@@ -1,26 +1,67 @@
 #include "ButtonComponent.h"
 #include "Engine/ResourceDefines.h"
 #include "Engine/GameObjects/Transform.h"
-#include "Engine/Managers/GraphicsManager.h"
 #include "Engine/EditorUI/EditorUI.h"
+#include "Engine/Managers/Events/EventManager.h"
+#include "Engine/Managers/Events/MouseEvents.h"
+#include "Engine/Managers/GameManager.h"
+#include "Engine/GameObjects/CameraGameObject.h"
 
 ButtonComponent::ButtonComponent(string identifier, CoolUUID uuid, XMFLOAT3& position, XMFLOAT3& scale, XMFLOAT3& rotation) : GameUIComponent(identifier, uuid, position, scale, rotation)
 {
 	m_componentType |= UIComponentType::BUTTON;
 
-	m_ppixelShader = GraphicsManager::GetInstance()->GetPixelShader(BUTTON_PIXEL_SHADER_NAME);
+	EventManager::Instance()->AddClient(EventType::MouseButtonPressed, this);
+	EventManager::Instance()->AddClient(EventType::MouseButtonReleased, this);
 }
 
 ButtonComponent::ButtonComponent(nlohmann::json& data, CoolUUID uuid) : GameUIComponent(data, uuid)
 {
 	m_componentType |= UIComponentType::BUTTON;
+
+	EventManager::Instance()->AddClient(EventType::MouseButtonPressed, this);
 }
 
-void ButtonComponent::SetTexture(std::wstring wsfilepath, ButtonTexture textureType)
+ButtonComponent::~ButtonComponent()
 {
-	m_pButtonTexture[int(textureType)] = GraphicsManager::GetInstance()->GetShaderResourceView(wsfilepath);
+	EventManager::Instance()->RemoveClientAllEvents(this);
+}
 
-	if (m_pButtonTexture[int(ButtonTexture::PRESSED)] == nullptr)
+void ButtonComponent::Update()
+{
+	XMFLOAT2 mouseWorldPos = GameManager::GetInstance()->GetCamera()->GetMousePositionInWorldSpace();
+
+	XMFLOAT3 buttonPosition = m_transform->GetWorldPosition();
+	XMFLOAT3 buttonScale = m_transform->GetWorldScale();
+
+	m_isHovered = false;
+	if (mouseWorldPos.x >= buttonPosition.x - (buttonScale.x) && mouseWorldPos.x <= buttonPosition.x + (buttonScale.x))
+	{
+		if (mouseWorldPos.y >= buttonPosition.y - (buttonScale.y) && mouseWorldPos.y <= buttonPosition.y + (buttonScale.y))
+		{
+			m_isHovered = true;
+		}
+	}
+
+	if (!m_isHovered)
+	{
+		SetButtonState(ButtonState::RELEASED);
+	}
+	else if (m_buttonClicked)
+	{
+		SetButtonState(ButtonState::PRESSED);
+	}
+	else
+	{
+		SetButtonState(ButtonState::HOVERED);
+	}
+}
+
+void ButtonComponent::SetTexture(std::wstring wsfilepath, ButtonState textureType)
+{
+	m_pButtonTextures[int(textureType)] = GraphicsManager::GetInstance()->GetShaderResourceView(wsfilepath);
+
+	if (m_pButtonTextures[int(textureType)] == nullptr)
 	{
 		LOG("Tried to get a texture that doesn't exist!");
 	}
@@ -28,6 +69,39 @@ void ButtonComponent::SetTexture(std::wstring wsfilepath, ButtonTexture textureT
 	{
 		m_buttonTexFilepath[int(textureType)] = wsfilepath;
 	}
+}
+
+void ButtonComponent::Handle(Event* e)
+{
+	switch (e->GetEventID())
+	{
+	case EventType::MouseButtonPressed:
+		if (((MouseButtonPressedEvent*)e)->GetButton() == VK_LBUTTON)
+		{
+			if (m_isHovered)
+			{
+				m_buttonClicked = true;
+			}
+		}
+		break;
+
+	case EventType::MouseButtonReleased:
+		if (((MouseButtonReleasedEvent*)e)->GetButton() == VK_LBUTTON)
+		{
+			if (m_buttonClicked && m_isHovered && m_OnClickFunction)
+			{
+				m_OnClickFunction(m_OnClickFunctionArg);
+			}
+			m_buttonClicked = false;
+		}
+		break;
+	}
+}
+
+void ButtonComponent::SetButtonOnClick(void(*onClick)(void*), void* argument)
+{
+	m_OnClickFunction = onClick;
+	m_OnClickFunctionArg = argument;
 }
 
 #if EDITOR
@@ -42,12 +116,18 @@ void ButtonComponent::CreateEngineUI()
 	ImVec2 textureSize = ImVec2(128, 32);
 	float columnWidth = 120.0f;
 
-	EditorUI::Texture("Pressed Texture", m_buttonTexFilepath[int(ButtonTexture::PRESSED)], m_pButtonTexture[int(ButtonTexture::PRESSED)], columnWidth, textureSize);
+	EditorUI::Texture("Released Texture", m_buttonTexFilepath[int(ButtonState::RELEASED)], m_pButtonTextures[int(ButtonState::RELEASED)], columnWidth, textureSize);
 	ImGui::Spacing();
-	EditorUI::Texture("Released Texture", m_buttonTexFilepath[int(ButtonTexture::RELEASED)], m_pButtonTexture[int(ButtonTexture::RELEASED)], columnWidth, textureSize);
+	EditorUI::Texture("Pressed Texture", m_buttonTexFilepath[int(ButtonState::PRESSED)], m_pButtonTextures[int(ButtonState::PRESSED)], columnWidth, textureSize);
 	ImGui::Spacing();
-	EditorUI::Texture("Hovered Texture", m_buttonTexFilepath[int(ButtonTexture::HOVERED)], m_pButtonTexture[int(ButtonTexture::HOVERED)], columnWidth, textureSize);
+	EditorUI::Texture("Hovered Texture", m_buttonTexFilepath[int(ButtonState::HOVERED)], m_pButtonTextures[int(ButtonState::HOVERED)], columnWidth, textureSize);
 
 	ImGui::Spacing();
 }
 #endif
+
+void ButtonComponent::SetButtonState(ButtonState buttonState)
+{
+	m_currentButtonState = buttonState;
+	m_ptexture = m_pButtonTextures[(int)buttonState];
+}
--- CoolEngine/Engine/GameUI/ButtonComponent.h
+++ CoolEngine/Engine/GameUI/ButtonComponent.h
@@ -1,30 +1,45 @@
 #pragma once
 #include "GameUIComponent.h"
+#include "Engine/Managers/Events/EventObserver.h"
 
 #define NUM_BUTTON_TEXTURES 3
 
-enum class ButtonTexture
+enum class ButtonState
 {
-	PRESSED,
-	RELEASED,
+	PRESSED = 0,
 	HOVERED,
-	COUNT	
+	RELEASED,
+	COUNT
 };
 
-class ButtonComponent : public GameUIComponent
+class ButtonComponent : public GameUIComponent, public Observer
 {
 	friend FileIO;
 private:
-	ID3D11ShaderResourceView* m_pButtonTexture[(int)ButtonTexture::COUNT];
-	wstring m_buttonTexFilepath[(int)ButtonTexture::COUNT];
+	ID3D11ShaderResourceView* m_pButtonTextures[(int)ButtonState::COUNT];
+	wstring m_buttonTexFilepath[(int)ButtonState::COUNT];
+
+	ButtonState m_currentButtonState = ButtonState::RELEASED;
+	bool m_isHovered = false;
+	bool m_buttonClicked = false;
+
+	void (*m_OnClickFunction)(void*) = nullptr;
+	void* m_OnClickFunctionArg = nullptr;
 public:
 	ButtonComponent(string identifier, CoolUUID uuid, XMFLOAT3& position, XMFLOAT3& scale, XMFLOAT3& rotation);
 	ButtonComponent(nlohmann::json& data, CoolUUID uuid);
+	~ButtonComponent();
 
-	void SetTexture(std::wstring wsfilepath, ButtonTexture textureType);
+	virtual void Update()override;
+
+	void SetTexture(std::wstring wsfilepath, ButtonState textureType);
+
+	void Handle(Event* e) override;
+	void SetButtonOnClick(void (*onClick)(void*), void* argument);
 
 #if EDITOR
 	void CreateEngineUI() override;
 #endif
-};
 
+	void SetButtonState(ButtonState buttonState);
+};
